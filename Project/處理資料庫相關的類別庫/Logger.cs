fa841b6780e9833d9f2fe319c408ee9e987188dd
who000238 <who000238@gmail.com>
2021-08-22 13:24:57 +0800

    增加Logger類別、登入功能method寫入、寫入登入CS頁面的程式碼 、增加一個管理使用者資料的類別(GetUserInfoByAccount)

---
+++ Project/處理資料庫相關的類別庫/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 處理資料庫相關的類別庫
+{
+    class Logger
+    {
+        public static void WriteLog(Exception ex)
+        {
+            string msg =
+                $@"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}
+                   {ex.ToString()}
+                ";
+
+            string logPath = "D:\\Logs\\Log.log";
+            string folderPath = System.IO.Path.GetDirectoryName(logPath);
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+
+            System.IO.File.AppendAllText("D:\\Logs\\Log.log", ex.ToString());
+
+            throw ex;
+        }
+    }
+}
