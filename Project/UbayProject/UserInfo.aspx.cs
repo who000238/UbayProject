9269a1c8b5c4f05a6ce5875d2af9737619536a23
hellotherepcw <jimmy50916@gmail.com>
2021-08-23 14:08:10 +0800

    UserInfo Page 增加控制項

--- Project/UbayProject/UserInfo.aspx.cs
+++ Project/UbayProject/UserInfo.aspx.cs
@@ -11,6 +11,23 @@ namespace UbayProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //取得目前登入使用者ID(Session)
+            //取得目前顯示使用者資料(QueryString)
+            //顯示在各個位置
+        }
+
+        protected void btnUpdateUserName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdateUserIntro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdateUserPhoto_Click(object sender, EventArgs e)
+        {
 
         }
     }
