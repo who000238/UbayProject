8a9404a45dbe5b4d2ded3f4898fa1f0ed84c464c
hellotherepcw <jimmy50916@gmail.com>
2021-08-27 15:08:06 +0800

    暫存

--- Project/UbayProject/UserInfo.aspx.designer.cs
+++ Project/UbayProject/UserInfo.aspx.designer.cs
@@ -96,13 +96,13 @@ namespace UbayProject
         protected global::System.Web.UI.WebControls.Button btnUpdateUserBirthday;
 
         /// <summary>
-        /// lblIntro 控制項。
+        /// txtUserIntro 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblIntro;
+        protected global::System.Web.UI.WebControls.TextBox txtUserIntro;
 
         /// <summary>
         /// btnUpdateUserIntro 控制項。
